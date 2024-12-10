6dd7e013af0d39b94f7d699545faa068e5f47ccb
aremzy03 <aremzy2018@gmail.com>
2024-12-10 17:21:34 +0100

    adding advanced task

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,10 @@
+monthly_income = int(input("Enter your monthly income: "))
+monthly_expense = int(input("Enter your monthly expenses: "))
+rate = 0.05
+
+monthly_savings = monthly_income - monthly_expense
+
+annual_savings = monthly_savings * 12 + (monthly_savings * 12 * rate)
+
+print(f"Your monthly savings are ${monthly_savings}")
+print(f"Projected savings after one year, with interest, is: ${annual_savings}.")
