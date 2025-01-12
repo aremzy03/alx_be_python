70865ed8ff7b8bd7347718785498031dfd3bddf2
aremzy03 <aremzy2018@gmail.com>
2025-01-12 21:25:14 +0100

    added a readme and done with task 0 and 1

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,12 +1,12 @@
 def perform_operation(num1, num2, operation):
-    match operation:
-        case "add":
-            return num1 + num2
-        case "subtract":
-            return num1 - num2
-        case "multiply":
-            return num1 * num2
-        case "divide":
-            if num1 == 0 or num2 == 0:
-                return "cannot divide by zero (0)."
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num1 == 0 or num2 == 0:
+            return "Cannot divide by zero (0)."
+        else:
             return num1 / num2
