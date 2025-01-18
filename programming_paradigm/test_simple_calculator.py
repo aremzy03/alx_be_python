085cc096c83bb1dce1a3c7273657c66a935e908d
aremzy03 <aremzy2018@gmail.com>
2025-01-18 19:37:54 +0100

    final task

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -2,24 +2,26 @@ import unittest
 from simple_calculator import SimpleCalculator
 
 class Testcalculator(unittest.TestCase):
-    def testadd(self):
+    
+    
+    def test_addition(self):
         self.assertEqual(SimpleCalculator().add(None, 1, 2), 3)
         self.assertEqual(SimpleCalculator().add(None, -1, 2), 1)
         self.assertEqual(SimpleCalculator().add(None, -3, -2), -5)
         
         
-    def testsub(self):
+    def test_subtraction(self):
         self.assertEqual(SimpleCalculator().subtract(None, 5, 2), 3)
         self.assertEqual(SimpleCalculator().subtract(None, -5, 2), -7)
         self.assertEqual(SimpleCalculator().subtract(None, 0, 2), -2)
     
         
-    def testmul(self):
+    def test_multiplication(self):
         self.assertEqual(SimpleCalculator().multiply(None, 2, 5), 10)
         self.assertEqual(SimpleCalculator().multiply(None, -2, 5), -10)
         self.assertEqual(SimpleCalculator().multiply(None, 0, 5), 0)
     
-    def testdiv(self):
+    def test_division(self):
         self.assertEqual(SimpleCalculator().divide(None, 10, 2), 5)
         self.assertEqual(SimpleCalculator().divide(None, 5, 2), 2.5)
         self.assertEqual(SimpleCalculator().divide(None, -10, 2), -5)
